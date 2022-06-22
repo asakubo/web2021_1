dfc87d34d2de33e5dbd64f917e42be49040fcbb5
隼輔 <107975530+asakubo@users.noreply.github.com>
2022-06-22 07:05:58 +0000

    表の結合

--- insert.js
+++ insert.js
@@ -2,13 +2,13 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test2.db');
 
 
-let sql = `
-insert into club ("name") values ("Mancher.C");
-`
+
 let sql = `
 insert into club ("name","club_id") values ("K.De Bryne","3");
 `
-
+let sql = `
+insert into club ("name") values ("Mancher.C");
+`
 
 
 
